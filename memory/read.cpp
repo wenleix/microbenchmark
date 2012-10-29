50953135236b92c7102c6220d778ae253dfb5735
Wenlei Xie <wenlei.xie@outlook.com>
2012-10-28 21:51:59 -0400

    update

---
+++ memory/read.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <sys/time.h>
+#include <cstring>
+
+using namespace std;
+
+#define STRIDE 16
+
+const int Size = 1024 * 1024 * 1024 / 4;  //    1G
+float c[Size];
+
+int main() {
+	for (int i = 0; i < Size; i++) {
+		c[i] = 0;
+	}
+
+	float sum = 0.0f;
+	float *cend = c + Size;
+	float *cptr = c;
+	
+    timeval start, end;
+    gettimeofday(&start, NULL);
+
+	while (cptr != cend) {
+		sum += *cptr;
+		cptr += STRIDE;
+	}
+
+    gettimeofday(&end, NULL);
+	printf("sum = %.3f\n", sum);    //  Avoid optimization.
+    double elapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+    elapsed /= 1000000;
+    printf("%.2f sec\n", elapsed);
+    printf("%.4f MB/s\n", Size * sizeof(float) / 1024 / 1024 / elapsed);
+    return 0;
+}
+
+
---
+++ memory/write.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <sys/time.h>
+#include <cstring>
+
+using namespace std;
+
+#define STRIDE 16
+
+const int Size = 1024 * 1024 * 1024 / 4;    // 1G
+float c[Size];
+
+int main() {
+	for (int i = 0; i < Size; i++) {
+		c[i] = 0;
+	}
+
+	float *cend = c + Size;
+	float *cptr = c;
+	
+    timeval start, end;
+    gettimeofday(&start, NULL);
+
+	while (cptr != cend) {
+		*cptr = 1.23f;
+		cptr += STRIDE;
+	}
+
+    gettimeofday(&end, NULL);
+    double elapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+    elapsed /= 1000000;
+    printf("%.2f sec\n", elapsed);
+    printf("%.4f MB/s\n", Size * sizeof(float) / 1024 / 1024 / elapsed);
+    return 0;
+}
+
+
