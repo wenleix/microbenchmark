59eab432568e605077c9e337eac6a99a88e949fc
Wenlei Xie <wenlei.xie@outlook.com>
2012-12-26 20:57:39 -0500

    code alignment

--- memory/cache/sequentialBlock.cpp
+++ memory/cache/sequentialBlock.cpp
@@ -12,58 +12,58 @@ const int InnerLoop = 20;
 int blockSize;
 
 void* workerThread(void* array) {
-	float* a = (float*)(array);
-	for (int offset = 0; offset < Length; offset += blockSize) {
-		float* b = a + offset;
-		//	Work on the block
-		int block = min(blockSize, Length - offset);
-		for (int i = 0; i < InnerLoop; i++) {
-			for (int j = 0; j < block; j++)
-				b[j] = b[j] * 2 + j;
-			float sum = 0.0;
-			for (int j = 0; j < block; j++)
-				sum += b[j];
-			sum /= blockSize;
-			for (int j = 0; j < block; j++)
-				b[j] -= sum;
-		}
-	}
+    float* a = (float*)(array);
+    for (int offset = 0; offset < Length; offset += blockSize) {
+        float* b = a + offset;
+        //	Work on the block
+        int block = min(blockSize, Length - offset);
+        for (int i = 0; i < InnerLoop; i++) {
+            for (int j = 0; j < block; j++)
+                b[j] = b[j] * 2 + j;
+            float sum = 0.0;
+            for (int j = 0; j < block; j++)
+                sum += b[j];
+            sum /= blockSize;
+            for (int j = 0; j < block; j++)
+                b[j] -= sum;
+        }
+    }
 }
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s [# Threads] [BlockSize(KB)]\n", argv[0]);
-		return 1;
-	}
-	int numThread = atoi(argv[1]);
-	blockSize = atoi(argv[2]) * 1024 / 4;
-	//fprintf(stderr, "blockSize %d\n", blockSize);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s [# Threads] [BlockSize(KB)]\n", argv[0]);
+        return 1;
+    }
+    int numThread = atoi(argv[1]);
+    blockSize = atoi(argv[2]) * 1024 / 4;
+    //fprintf(stderr, "blockSize %d\n", blockSize);
 
-	fprintf(stdout, "# Threads: %d, BlockSize %d KB !\n", numThread, atoi(argv[2]));
-	float* s[100];
-	for (int i = 0; i < numThread; i++) {
-		s[i] = new float[Length];
-		memset(s[i], 0, sizeof(float) * Length);
-	}
+    fprintf(stdout, "# Threads: %d, BlockSize %d KB !\n", numThread, atoi(argv[2]));
+    float* s[100];
+    for (int i = 0; i < numThread; i++) {
+        s[i] = new float[Length];
+        memset(s[i], 0, sizeof(float) * Length);
+    }
 
     timeval start, end;
     gettimeofday(&start, NULL);
 
-	pthread_t p[100];
-	for (int i = 0; i < numThread; i++) 
-		pthread_create(p + i, NULL, workerThread, (void*)(s[i]));
-	for (int i = 0; i < numThread; i++)
-		pthread_join(p[i], NULL);
+    pthread_t p[100];
+    for (int i = 0; i < numThread; i++) 
+        pthread_create(p + i, NULL, workerThread, (void*)(s[i]));
+    for (int i = 0; i < numThread; i++)
+        pthread_join(p[i], NULL);
 
     gettimeofday(&end, NULL);
-    
-	double elapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+
+    double elapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
     elapsed /= 1000000;
     printf("%.2f sec\n", elapsed);
 
-	for (int i = 0; i < numThread; i++)
-		delete [] s[i];
+    for (int i = 0; i < numThread; i++)
+        delete [] s[i];
 
-	return 0;
+    return 0;
 }
 
